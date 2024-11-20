7961877153f7f41ce1fab5967b4cb86ac4406d6b
JCarvajalLab <carvajal.jordan94@gmail.com>
2024-11-20 18:40:03 -0300

    comenzando a crear el curriculum por cdn vue

---
+++ asset/main.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        nombre: "Jordan",
+        apellidos: "Carvajal",
+        direccion: "El Salvador",
+        telefono: "930458517",
+        correo: "jcarvajal@gmail.com",
+        foto:"https://st.depositphotos.com/1158045/3239/i/450/depositphotos_32391649-stock-photo-businessman-outdoor-in-the-city.jpg",
+    },
+});
